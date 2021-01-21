78f800a3a3e9244444f7de6a5813659762fdbf8c
Gildas LAVERDURE <glaverdu@e3r10p21.42.fr>
2021-01-21 13:52:23 +0100

    bite

---
+++ get_next_line.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glaverdu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/19 12:39:16 by glaverdu          #+#    #+#             */
+/*   Updated: 2021/01/20 16:21:44 by glaverdu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	get_next_line(int fd, char **line)
+{
+	char			*buf;
+	int				ret;
+	static char		*str;
+
+	if (!line || fd == -1 || BUFFER_SIZE <= 0)
+		return (-1);
+	if (!(buf = malloc(sizeof(char*) * BUFFER_SIZE + 1)))
+		return (-1);
+	ret = 1;
+	while ((check_nl(str) && ret != 0))
+	{
+		if ((ret = read(fd, buf, BUFFER_SIZE)) == -1)
+		{
+			free(buf);
+			return (-1);
+		}
+		buf[ret] = '\0';
+		str = ft_strjoin(str, buf);
+	}
+	free(buf);
+	*line = cpystatic(str);
+	str = cpyend(str);
+	if (ret == 0)
+		return (0);
+	return (1);
+}
+
+int main()
+{
+	int fd;
+	char *line;
+	line = malloc(sizeof(char*) * 100);
+	fd = open("42", O_RDWR);
+	get_next_line(fd, &line);
+	free(line);
+	get_next_line(fd, &line);
+	free(line);
+	get_next_line(fd, &line);
+	free(line);
+	get_next_line(fd, &line);
+	free(line);
+	get_next_line(fd, &line);
+	get_next_line(fd, &line);
+	while(1){;}
+}
